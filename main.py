13dabb179874bdf49bf9ca25a491dcdd7eb88396
Nabyne <144941710+Nabyne1337@users.noreply.github.com>
2023-11-29 02:52:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -73,129 +73,132 @@ class Browser(QMainWindow):
             # JavaScript-код, который будет выполнен при нажатии кнопки sta rt 
             js_code = """
             function main() {
-    //check if game is being played
-    let chessboard = document.querySelector("chess-board");
+    let chessboard = document.querySelector("wc-chess-board");
     if (chessboard == null || !chessboard) {
-        return { status: "false", error: "Не удаётся запустить скрипт. Пожалуйста делайте это тогда в самой игре или попробуйте умереть." }
+        return { status: "false", error: "Не удаётся запустить скрипт. Пожалуйста, делайте это тогда в самой игре или попробуйте умереть." };
     }
-    var player_colour = 'white'
-    var player_colour = prompt("Ты играешь за white или black ?")
-    while (player_colour != "white" && player_colour != "black") {
-        player_colour = prompt("Ты играешь за white или black ?")
+
+    var player_colour = 'white';
+    player_colour = prompt("Ты играешь за white или black ?");
+    while (player_colour !== "white" && player_colour !== "black") {
+        player_colour = prompt("Ты играешь за white или black ?");
     }
-    player_colour = player_colour.split("")[0]
-    //generate FEN string from board,
+    player_colour = player_colour.charAt(0);
+
     function getFenString() {
-        let fen_string = ""
-        for (var i = 8; i >= 1; i--) {
-            for (var j = 1; j <= 8; j++) {
-                let position = `${j}${i}`
-                //for every new row on the chessboard
-                if (j == 1 && i != 8) {
-                    fen_string += "/"
+        let fen_string = "";
+        for (let i = 8; i >= 1; i--) {
+            for (let j = 1; j <= 8; j++) {
+                let position = `${j}${i}`;
+                if (j === 1 && i !== 8) {
+                    fen_string += "/";
                 }
-                let piece_in_position = document.querySelectorAll(`.piece.square-${position}`)[0]?.classList ?? null
-                //get piece name by shortest class
-                if (piece_in_position != null) {
-                    for (var item of piece_in_position.values()) {
-                        if (item.length == 2) {
-                            piece_in_position = item
+
+                let piece_in_position = document.querySelectorAll(`.piece.square-${position}`)[0]?.classList ?? null;
+
+                if (piece_in_position !== null) {
+                    for (let item of piece_in_position.values()) {
+                        if (item.length === 2) {
+                            piece_in_position = item;
                         }
                     }
                 }
-                //if position is empty
-                if (piece_in_position == null) {
-                    //if previous position is empty, sum up numbers
-                    let previous_char = fen_string.split("").pop()
-                    if (!isNaN(Number(previous_char))) {
-                        fen_string = fen_string.substring(0, fen_string.length - 1)
-                        fen_string += Number(previous_char) + 1
-                    }
-                    else {
-                        fen_string += "1"
-                    }
-                }
-                else if (piece_in_position?.split("")[0] == "b") {
-                    fen_string += piece_in_position.split("")[1]
-                }
-                else if (piece_in_position?.split("")[0] == "w") {
-                    fen_string += piece_in_position.split("")[1].toUpperCase()
-                }
 
+                if (piece_in_position === null) {
+                    let previous_char = fen_string.split("").pop();
+                    if (!isNaN(Number(previous_char))) {
+                        fen_string = fen_string.substring(0, fen_string.length - 1);
+                        fen_string += Number(previous_char) + 1;
+                    } else {
+                        fen_string += "1";
+                    }
+                } else if (piece_in_position?.split("")[0] === "b") {
+                    fen_string += piece_in_position.split("")[1];
+                } else if (piece_in_position?.split("")[0] === "w") {
+                    fen_string += piece_in_position.split("")[1].toUpperCase();
+                }
             }
         }
-        return fen_string
+        return fen_string;
     }
-    let fen_string = getFenString()
-    fen_string += ` ${player_colour}`
-    console.log(fen_string)
-    const engine = new Worker("/bundles/app/js/vendor/jschessengine/stockfish.asm.1abfa10c.js")
-    engine.postMessage(`position fen ${fen_string}`)
+
+    let fen_string = getFenString();
+    fen_string += ` ${player_colour}`;
+    console.log(fen_string);
+
+    const engine = new Worker("/bundles/app/js/vendor/jschessengine/stockfish.asm.1abfa10c.js");
+    engine.postMessage(`position fen ${fen_string}`);
     engine.postMessage('go wtime 300000 btime 300000 winc 2000 binc 2000');
-    engine.postMessage("go debth 99")
-    //listen for when moves are made
+    engine.postMessage("go debth 99");
+
     setInterval(() => {
-        let new_fen_string = getFenString()
-        new_fen_string += ` ${player_colour}`
-        if (new_fen_string != fen_string) {
-            fen_string = new_fen_string
-            engine.postMessage(`position fen ${fen_string}`)
+        let new_fen_string = getFenString();
+        new_fen_string += ` ${player_colour}`;
+        if (new_fen_string !== fen_string) {
+            fen_string = new_fen_string;
+            engine.postMessage(`position fen ${fen_string}`);
             engine.postMessage('go wtime 300000 btime 300000 winc 2000 binc 2000');
-            engine.postMessage("go debth 99")
+            engine.postMessage("go debth 99");
         }
-    })
+    });
+
     engine.onmessage = function (event) {
         if (event.data.startsWith('bestmove')) {
             const bestMove = event.data.split(' ')[1];
-            char_map = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8 }
-            console.log('Best move:', bestMove);
-            document.getElementById("best-move").innerHTML = ` Лучший ход это ${bestMove}`
-            //create cheat squares on the board
-            previous_cheat_squares = document.querySelectorAll(".cheat-highlight").forEach((element) => {
-                //remove all previous cheat squares
-                element.remove()
-            })
-            bestMove_array = bestMove.split("")
-            initial_position = `${char_map[bestMove_array[0]]}${bestMove_array[1]}`
-            final_position = `${char_map[bestMove_array[2]]}${bestMove_array[3]}`
+            const char_map = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8 };
+            document.getElementById("best-move").innerHTML = ` Лучший ход это ${bestMove}`;
 
-            initial_highlight = document.createElement("div");
-            initial_highlight.className = `highlight cheat-highlight square-${initial_position}`
-            initial_highlight.style = "background:blue;opacity:0.2;border: 5px outset white;color:white"
-            initial_highlight.innerHTML = "" 
-            //outset
-            final_highlight = document.createElement("div");
-            final_highlight.className = `highlight cheat-highlight square-${final_position}`
-            final_highlight.style = "background:red;opacity:0.3;border: 5px outset white;color:white"
-            final_highlight.innerHTML = ""
-            document.querySelector("chess-board").appendChild(initial_highlight)
-            document.querySelector("chess-board").appendChild(final_highlight)
+            document.querySelectorAll(".cheat-highlight").forEach((element) => {
+                element.remove();
+            });
+
+            const bestMoveArray = bestMove.split("");
+            const initialPosition = `${char_map[bestMoveArray[0]]}${bestMoveArray[1]}`;
+            const finalPosition = `${char_map[bestMoveArray[2]]}${bestMoveArray[3]}`;
+
+            const initialHighlight = document.createElement("div");
+            initialHighlight.className = `highlight cheat-highlight square-${initialPosition}`;
+            initialHighlight.style.background = "blue";
+            initialHighlight.style.opacity = 0.2;
+            initialHighlight.style.border = "5px outset white";
+            initialHighlight.style.color = "white";
+            initialHighlight.innerHTML = "";
+
+            const finalHighlight = document.createElement("div");
+            finalHighlight.className = `highlight cheat-highlight square-${finalPosition}`;
+            finalHighlight.style.background = "red";
+            finalHighlight.style.opacity = 0.3;
+            finalHighlight.style.border = "5px outset white";
+            finalHighlight.style.color = "white";
+            finalHighlight.innerHTML = "";
+
+            chessboard.appendChild(initialHighlight);
+            chessboard.appendChild(finalHighlight);
         }
-    }
-    return { status: true }
+    };
 
+    return { status: true };
 }
+
 function startHack(element) {
-    element.innerHTML = "Пожалуйста подождите.."
-    element.disabled = true
-    let hack = main()
-    if (hack.status == true) {
-        element.innerHTML = `<span id = 'best-move'>Рассчёт лучшего хода..</span>`
-    }
-    else {
-        element.innerHTML = "Start Hack"
-        element.disabled = false
-        alert(hack.error)
+    element.innerHTML = "Пожалуйста подождите..";
+    element.disabled = true;
+    let hack = main();
+    if (hack.status === true) {
+        element.innerHTML = `<span id='best-move'>Рассчёт лучшего хода..</span>`;
+    } else {
+        element.innerHTML = "Start Hack";
+        element.disabled = false;
+        alert(hack.error);
     }
 }
-
 
 var button = document.createElement("button");
-button.className = "ui_v5-button-full"
-button.innerHTML = "Запустить"
-button.onclick = () => { startHack(button) }
-let main_body = document.querySelector(".board-layout-main")
-main_body.prepend(button)
+button.className = "ui_v5-button-full";
+button.innerHTML = "Запустить";
+button.onclick = () => { startHack(button); };
+let main_body = document.querySelector(".board-layout-main");
+main_body.prepend(button);
 """
             self.browser.page().runJavaScript(js_code)
         else:
